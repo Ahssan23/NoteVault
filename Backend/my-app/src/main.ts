c4580a3be56368afd07455b4a68c128edbc30c9c
Ahssan23 <ahssan180sx@gmail.com>
2026-05-17 16:01:59 +0500

    Update main.ts

--- Backend/my-app/src/main.ts
+++ Backend/my-app/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: 'http://localhost:4000',
+  origin: 'https://notevault-1-pmjq.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 })
